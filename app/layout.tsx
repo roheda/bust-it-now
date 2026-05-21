a39e3b7d489755e3e082db889f7ded87ff33f29a
roheda <rodrigoherreradavid@gmail.com>
2026-05-20 20:15:43 -0600

    Show feedback widget across app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import FeedbackWidget from "./components/FeedbackWidget";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,7 +29,10 @@ export default function RootLayout({
       lang="es"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <FeedbackWidget />
+      </body>
     </html>
   );
 }
