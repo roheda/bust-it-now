5711e2eaa960d11901b2879757382161c5f9bf5e
roheda <rodrigoherreradavid@gmail.com>
2026-06-04 17:35:13 -0600

    Use new logo overlay editor widget

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import FeedbackWidget from "./components/FeedbackWidget";
 import HideBriefLogoControls from "./components/HideBriefLogoControls";
-import LogoOverlayGlobalWidget from "./components/LogoOverlayGlobalWidget";
+import LogoOverlayEditorWidget from "./components/LogoOverlayEditorWidget";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -34,7 +34,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         {children}
         <HideBriefLogoControls />
-        <LogoOverlayGlobalWidget />
+        <LogoOverlayEditorWidget />
         <FeedbackWidget />
       </body>
     </html>
