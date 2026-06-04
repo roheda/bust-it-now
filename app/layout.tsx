9092a14ab94fc1789cf1c27e641fbc5031f383f1
roheda <rodrigoherreradavid@gmail.com>
2026-06-04 16:42:36 -0600

    Mount helper to hide brief logo controls

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import FeedbackWidget from "./components/FeedbackWidget";
+import HideBriefLogoControls from "./components/HideBriefLogoControls";
 import LogoOverlayGlobalWidget from "./components/LogoOverlayGlobalWidget";
 import "./globals.css";
 
@@ -32,6 +33,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         {children}
+        <HideBriefLogoControls />
         <LogoOverlayGlobalWidget />
         <FeedbackWidget />
       </body>
