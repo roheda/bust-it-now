f8fd7e55414295e82695a80a401b7a672624bedd
roheda <rodrigoherreradavid@gmail.com>
2026-06-04 14:39:42 -0600

    Mount logo overlay button globally

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import FeedbackWidget from "./components/FeedbackWidget";
+import LogoOverlayGlobalWidget from "./components/LogoOverlayGlobalWidget";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -31,6 +32,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         {children}
+        <LogoOverlayGlobalWidget />
         <FeedbackWidget />
       </body>
     </html>
