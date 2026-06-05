7062e5029eda3d395a4c6cdd36a3ca77bea3f54a
roheda <rodrigoherreradavid@gmail.com>
2026-06-04 20:35:15 -0600

    Mount brief asset enhancer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import EnhanceBriefAssets from "./components/EnhanceBriefAssets";
 import FeedbackWidget from "./components/FeedbackWidget";
 import HideBriefLogoControls from "./components/HideBriefLogoControls";
 import LogoOverlayEditorWidget from "./components/LogoOverlayEditorWidget";
@@ -34,6 +35,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         {children}
         <HideBriefLogoControls />
+        <EnhanceBriefAssets />
         <LogoOverlayEditorWidget />
         <FeedbackWidget />
       </body>
